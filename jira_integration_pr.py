1c9f03396c9d1a8b6ed40cbab90e5a19ceb507d5
Jonas Katins <jonas.katins@baltech.de>
2022-09-20 17:26:27 +0200

    [bugfix] Fehlerhafte Links durch eckige Klammern in PR Titeln
    Der Link zum PR wird hiermit vom PR Titel zum Text "pull request" verschoben.
    
    ORG-211

--- jira_integration_pr.py
+++ jira_integration_pr.py
@@ -23,11 +23,11 @@ def convert_to_comment(author_name, pr_title, pr_url, branch_name,
                        base_branch_name, project_name, project_url):
     comment = io.StringIO()
     comment.write(
-        f"(*b) *{author_name}* opened a *pull request* "
+        f"(*b) *{author_name}* opened a [*pull request*|{pr_url}] "
         f"in [{project_name}|{project_url}] "
         f"for branch *{base_branch_name}* ← *{branch_name}*:"
         "{quote}"
-        f"[{pr_title}|{pr_url}]"
+        f"{pr_title}"
         "{quote}\n"
     )
     return comment.getvalue()
