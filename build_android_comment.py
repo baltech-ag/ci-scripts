87892db5539a1a5b5d0de58fd0e08e42054b7878
Jonas Katins <jonas.katins@baltech.de>
2024-03-18 15:57:00 +0100

    [internal] Möglichkeit Jira Ticket ID zu überschreiben
    QR Codes für Android Builds auf dem master Branch sollen in einem vom App-Repo
    statisch übergebenen Ticket gepostet werden, anstatt in den Tickets, die
    dynamisch in den betroffenen Commit Messages gefunden werden.

--- build_android_comment.py
+++ build_android_comment.py
@@ -14,6 +14,7 @@ NUM_COMMITS = 'CI_NUM_COMMITS'
 BRANCH_NAME = 'CI_COMMIT_REF_NAME'
 PROJECT_NAME = 'CI_PROJECT_NAME'
 PROJECT_DIR = 'CI_PROJECT_DIR'
+JIRA_TICKET_ID = 'CI_JIRA_TICKET_ID'
 ANDROID_URL = 'CI_ANDROID_URL'
 ANDROID_QRCODE = 'CI_ANDROID_QRCODE'
 
@@ -29,11 +30,14 @@ def convert_to_comment(author_name, project_name, repo_url, branch_name,
 
 def create_jira_comments():
     env = os.environ
-    all_commits = retrieve_commits(
-        env[PROJECT_DIR],
-        'HEAD~' + env[NUM_COMMITS],
-        env[CUR_COMMIT])
-    affected_issues = group_by_issue(all_commits).keys()
+    if env[JIRA_TICKET_ID]:
+        affected_issues = [env[JIRA_TICKET_ID]]
+    else:
+        all_commits = retrieve_commits(
+            env[PROJECT_DIR],
+            'HEAD~' + env[NUM_COMMITS],
+            env[CUR_COMMIT])
+        affected_issues = group_by_issue(all_commits).keys()
     comment = convert_to_comment(
         env[AUTHOR_NAME],
         env[PROJECT_NAME],
